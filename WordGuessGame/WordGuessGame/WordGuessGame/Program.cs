6f283162dd478e5733232ecf5c7166a3ee2986c6
Luay <luayghazwan@yahoo.com>
2017-12-21 11:47:54 -0800

    Start guess game method with welcome message

--- WordGuessGame/WordGuessGame/WordGuessGame/Program.cs
+++ WordGuessGame/WordGuessGame/WordGuessGame/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace WordGuessGame
 {
     class Game
     {
+
+        /// <summary>
+        /// This method presents the interface to the user with a welcome message and invoking the menu options.
+        /// </summary>
+        static void StartGuessGame()
+        {
+            Console.WriteLine("\n----------------------- Welcome to Word Guess Game! --------------------------" +
+                              "\n In this game you have to guess the right word by entering a single character at a time" +
+                              "\n When you try ten characters before guessing the mystery phrase, you lose the round." +
+                              "\n---------------------- Select an option from the Menu ------------------------");
+            //GameMenu();
+            Console.Read();
+        }
+
+        /// <summary>
+        /// The main point entry for the application program.cs ... This starts the game by calling the Start method.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartGuessGame();
         }
     }
 }
