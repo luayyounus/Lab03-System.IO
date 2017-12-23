9017bbb84be9b70ea2f8795e468e631309412f62
Luay <luayghazwan@yahoo.com>
2017-12-23 06:43:22 -0800

    Initial commit - Guess Game version 2

---
+++ WordGuessGame/WordGuessGame/GuessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
