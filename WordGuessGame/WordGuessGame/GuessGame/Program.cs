eeeef09037d127d1da3e48bdf0dea2cc6a1446f6
Luay <luayghazwan@yahoo.com>
2017-12-23 06:47:21 -0800

    Init game - welcome message and calling menu options

--- WordGuessGame/WordGuessGame/GuessGame/Program.cs
+++ WordGuessGame/WordGuessGame/GuessGame/Program.cs
@@ -4,9 +4,28 @@ namespace GuessGame
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main point entry for the application program.cs ... This initialize the game for the user to start.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitGame();
+        }
+
+        /// <summary>
+        /// This method includes the welcome message and the game instructions. It also invokes the Game Menu.
+        /// </summary>
+        private static void InitGame()
+        {
+            Console.WriteLine("\n----------------------- Welcome to Word Guess Game! --------------------------" +
+                              "\n In this game you have to guess the right word by entering a single character at a time" +
+                              "\n When you try ten characters before guessing the mystery phrase, you lose the round.");
+            bool gameOn = true;
+            while (gameOn)
+            {
+                //gameOn = ShowGameMenu();
+            }
         }
     }
 }
